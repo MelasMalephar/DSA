cf742b41d5e907f8d7555cdb4d068f57fdf71a2e
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2021-10-29 22:35:13 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.62%) - LeetHub

---
+++ number-complement/number-complement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        unsigned m = ~0;
+        while (n & m) m <<= 1;
+        return ~m & ~n;
+    }
+};
+
+// For example,
+
+// num          = 00000101
+// mask         = 11111000
+// ~mask & ~num = 00000010
