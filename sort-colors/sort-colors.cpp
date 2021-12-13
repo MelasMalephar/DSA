f7c405a264cde7a102d4b9d00e91e3e8e9e4c7f3
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2021-12-13 18:27:08 +0530

    Time: 4 ms (44.88%), Space: 8.2 MB (91.27%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void sortColors(vector<int>& v) {
-        int j=v.size()-1;
-        vector<int> k,l,m;
-        for(int i=j;i>=0;i--)
-        { if(v[i] == 2) { k.push_back(v[i]);} 
-          else if(!(v[i]&1)) 
-          {m.push_back(v[i]);}
-         else { l.push_back(v[i]);}
-        }
-       l.insert(l.end(),k.begin(),k.end()); 
-       m.insert(m.end(),l.begin(),l.end());
-       v=m;
+        int arr[3]={0,0,0};
+        for(auto& i:v)
+         { arr[i]++;}
+        v.clear();
+        for(int i=0;i<3;i++)
+        { while(arr[i]-- > 0){ v.push_back(i);}
+         }
     }
 };
