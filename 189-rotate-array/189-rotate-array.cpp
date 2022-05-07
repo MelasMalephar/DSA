6e1d4a187e4fd4bca7971bc7ec87819ca54a6d67
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-05-07 16:57:19 +0530

    Time: 42 ms (44.03%), Space: 26.2 MB (8.14%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> v(nums.size());
-        for(int i=0;i<nums.size();i++){
-            v[(i+k)%nums.size()] = nums[i];
-        }
-        nums = v;
+        int n=nums.size();
+        k%=nums.size();
+        auto it=next(nums.end(),-k);
+        // cout<<*it<<endl;
+        vector<int> v(it,nums.end());
+        // nums.erase(it,nums.end());
+        nums.insert(nums.begin(),v.begin(),v.end());
+        nums.resize(n);
+        
     }
 };
