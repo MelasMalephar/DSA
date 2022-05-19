793bb670da549d7b3451474a42ba974149c162a4
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-05-19 09:22:49 +0530

    Time: 18 ms (86.78%), Space: 16.9 MB (53.28%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int s=0;
-        for(auto &&i:nums)
-            s=s^i;
-        return s;
+        int i=1;
+        for(auto &c:nums){
+            i=i^c;
+        }
+        return i^1;
     }
 };
