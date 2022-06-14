0bdc2ff339b3241f986b9b98c78a9e942a3c654d
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-06-14 10:42:03 +0530

    Time: 133 ms (11.26%), Space: 7.5 MB (85.76%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public: int arr[501][501];
     int minDistance(string s, string t) {
-        memset(arr,0,sizeof(arr));
+        // memset(arr,0,sizeof(arr));
         int n=s.size(),m=t.size();
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(s[i-1]==t[j-1]) arr[i][j]=1+arr[i-1][j-1];
-                else arr[i][j]=max(arr[i-1][j],arr[i][j-1]);
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if(!i || !j) arr[i][j]=0;
+                else{
+                      if(s[i-1]==t[j-1]) arr[i][j]=1+arr[i-1][j-1];
+                      else arr[i][j]=max(arr[i-1][j],arr[i][j-1]);
+                }
             }
         }
         // cout<<arr[n][m]<<endl;
