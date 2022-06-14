45f63a6b445a32998ba927c2588a3a09138dcc87
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-06-14 10:37:36 +0530

    Time: 141 ms (10.74%), Space: 7.5 MB (85.76%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,15 +1,16 @@
 class Solution {
-public:
-    int minDistance(string word1, string word2) {
-        vector<vector<int> >dp(size(word1) + 1, vector<int>(size(word2) + 1));
-        for(int i = 0; i <= size(word1); i++) 
-         for(int j = 0; j <= size(word2); j++) 
-          if(!i || !j) dp[i][j] = i + j; // if one word == "", 
-                                                //all other characters of other word need to be deleted
-                // if characters match  ?  dont delete     : 1 deletion +
-                                                    //min to equalize after deletion of (word1[i], word2[j])
-          else 
-            dp[i][j] = word1[i - 1] == word2[j - 1] ? dp[i - 1][j - 1] : 1 + min(dp[i - 1][j], dp[i][j - 1]);
-        return dp[size(word1)][size(word2)];
+public: int arr[501][501];
+    int minDistance(string s, string t) {
+        memset(arr,0,sizeof(arr));
+        int n=s.size(),m=t.size();
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s[i-1]==t[j-1]) arr[i][j]=1+arr[i-1][j-1];
+                else arr[i][j]=max(arr[i-1][j],arr[i][j-1]);
+            }
+        }
+        // cout<<arr[n][m]<<endl;
+        int k=(m-arr[n][m])+(n-arr[n][m]);
+        return k;
     }
 };
