3600a8b4a37aa699f105059666fa7c5cbf80a65f
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-06-16 23:41:49 +0530

    Time: 550 ms (22.57%), Space: 18.6 MB (48.65%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if (s.size() == 1) return s;
-        int min_start = 0, max_len = 1;
-        for (int i = 0; i < s.size();) {
-          if (s.size() - i <= max_len / 2) break;
-          int j = i, k = i;
-          while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
-          i = k+1;
-          while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
-          int new_len = k - j + 1;
-          if (new_len > max_len) { min_start = j; max_len = new_len; }
+        int n=s.size()+1;
+        int dp[n][n];
+        memset(dp,0,sizeof(dp));
+        if(n==1)
+            return s;
+        
+        //fill 1 length and 2 length substring values
+        for(int i=0; i<n; i++){
+            dp[i][i] = 1; //single letter is palindrome
+            if(i==n-1) break;
+            
+            dp[i][i+1] = (s[i] == s[i+1]); // for ab check if (a== b)
         }
-    return s.substr(min_start, max_len);
+        
+        //fill substring which has length >= 3 
+        //for example-> aba (length = 3) string will be palindromic if s[0] == s[2] && nonboundary characters substring is also palindrome(that can be checked by dp[])
+        for(int i= n-3; i>=0; --i){
+            for(int j= i+2; j<n; ++j){
+                dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+            }
+        }
+        
+        int mx = INT_MIN;   //stores max length
+        string maxStr = ""; //stores longest palindromic substring
+        
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(dp[i][j]==1 && j-i+1>mx){
+                    mx = j-i+1;
+                    maxStr = s.substr(i, j-i+1);
+                }        
+            }
+        }
+        
+        return maxStr;
     }
 };
