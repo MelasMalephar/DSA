8e623bca2789006a158f18e0e60b40256d392743
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-07-06 11:35:14 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.23%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+       int a[n+4];
+       a[0]=0; 
+       a[1]=1; a[2]=1;
+       for(int i=3;i<n+1;i++){
+           a[i]=a[i-1]+a[i-2];
+       } 
+    return a[n];
     }
 };
