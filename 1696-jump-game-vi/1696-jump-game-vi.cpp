b3a82d213a08d070fe4e8c9ef5c6cf70cee578a6
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-07-09 22:06:25 +0530

    Time: 682 ms (5.72%), Space: 135.6 MB (18.68%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> ans(n,0);
+        
+        multiset<int> s;
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0)ans[i]=nums[i];
+            else{
+                auto it=s.end();
+                ans[i]=nums[i]+*(--it);
+            }
+            if(s.size()==k){ s.erase(s.find(ans[i+k]));}
+            s.insert(ans[i]);
+        }
+        // for(auto c:ans)cout<<c<<" ";
+        return ans[0];
+    }
+};
