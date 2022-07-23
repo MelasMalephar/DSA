2515341a61c47cfa0dc0e149c01e4b72daf7bc20
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-07-23 11:24:19 +0530

    Time: 1067 ms (33.95%), Space: 211.6 MB (30.67%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,23 +1,75 @@
 class Solution {
 public:
-    #define iterator vector<vector<int>>::iterator
-    void sort_count(iterator l, iterator r, vector<int>& count) {
-        if (r - l <= 1) return;
-        iterator m = l + (r - l) / 2;
-        sort_count(l, m, count);
-        sort_count(m, r, count);
-        for (iterator i = l, j = m; i < m; i++) {
-            while (j < r && (*i)[0] > (*j)[0]) j++;
-            count[(*i)[1]] += j - m; // add the number of valid "j"s to the indices of *i
+    vector<int> ans;
+    
+    void merge_function(vector<pair<int, int>>& arr, int left, int mid, int right)
+    {
+        vector<pair<int, int>> temp(right - left + 1);
+        int i = left;       
+        int j = mid + 1;        
+        int k = 0;        
+        int smaller_on_right = 0;        
+        while(i <= mid && j <= right)
+        {
+            if(arr[i].first <= arr[j].first)
+            {
+                ans[arr[i].second] += smaller_on_right; 
+                temp[k++] = arr[i++];
+            }
+            else
+            {
+                smaller_on_right++;               
+                temp[k++] = arr[j++];
+            }
         }
-        inplace_merge(l, m, r);
+        
+        while(i <= mid)
+        {
+            ans[arr[i].second] += smaller_on_right;
+            temp[k++] = arr[i++];
+        }
+        
+        while(j <= right)
+        {
+            temp[k++] = arr[j++];
+        }
+        
+        k = 0;
+        
+        for(int i = left; i <= right; i++)
+        {
+            arr[i] = temp[k++];
+        }
     }
+    
+    // function for merge sort
+    
+    void merge_sort(vector<pair<int, int>>& arr, int left, int right)
+    {
+        if(left < right)
+        {
+            int mid = left + (right - left) / 2;
+            
+            merge_sort(arr, left, mid);
+            
+            merge_sort(arr, mid + 1, right);
+            
+            merge_function(arr, left, mid, right);
+        }
+    }
+    
     vector<int> countSmaller(vector<int>& nums) {
-        vector<vector<int>> hold;
+        
         int n = nums.size();
-        for (int i = 0; i < n; ++i) hold.push_back(vector<int>({nums[i], i})); // "zip" the nums with their indices
-        vector<int> count(n, 0);
-        sort_count(hold.begin(), hold.end(), count);
-        return count;
+        
+        vector<pair<int, int>> arr(n);
+        
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = {nums[i], i};
+        }
+        ans.assign(n, 0);
+        merge_sort(arr, 0, n - 1);
+        return ans;
     }
 };
