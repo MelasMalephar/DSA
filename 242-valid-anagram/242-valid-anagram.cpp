c742e67111ff881797d5b187f98d08d6460cc5d2
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-07-28 09:58:21 +0530

    Time: 4 ms (95.45%), Space: 7.4 MB (7.79%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        unordered_map<char,int> mp;
+        for(auto &c:s)mp[c]++;
+        for(auto &c:t)if(--mp[c] <0) return false;
+        return true;
+    }
+};
