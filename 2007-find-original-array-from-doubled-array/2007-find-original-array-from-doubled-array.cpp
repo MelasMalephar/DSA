505e27be98383910903baa0a87f1ba3f4dd8b761
Namit Sharma <79407673+MelasMalephar@users.noreply.github.com>
2022-09-15 19:48:40 +0530

    Time: 833 ms (30.04%), Space: 144.1 MB (56.00%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& v) {
+        vector<int> ans;
+        if(v.size()%2 != 0) return ans;
+        map<int, int> mp;
+        for(auto num : v){
+            mp[num]++;
+        }
+        for(auto [k,l]:mp){
+            if(l==0) continue;
+            if(k==0){
+                if(l%2!=0) return ans;
+                for(int i=0;i<l/2;i++) ans.push_back(0);
+            }
+            else{
+                if(mp[2*k]>=l){
+                    mp[2*k]-=l;
+                    for(int i=0;i<l;i++){ ans.push_back(k);}
+                }
+                else return {};
+            }
+        }
+        return ans;
+        
+    }
+};
